c931230d44b43e3935090b3962f3b7f63d2cff1f
amvrogor <110409915+amvrogor@users.noreply.github.com>
2023-04-15 22:53:41 +0000

    Deploying to gh-pages from @ amvrogor/goit-react-hw-08-phonebook@e48428514e27f128e6b58eb14e929a05b9c06eb3 🚀

---
+++ static/js/415.a3be5686.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(3329),i={container:{minHeight:"calc(100vh - 300px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:600,fontSize:48,textAlign:"center",color:"#ee8905"}};function o(){return(0,r.jsx)("div",{style:i.container,children:(0,r.jsx)("h1",{style:i.title,children:"Another phonebook on the shelf"})})}}}]);
+//# sourceMappingURL=415.a3be5686.chunk.js.map
